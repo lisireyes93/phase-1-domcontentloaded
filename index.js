53cbb91dd5db86d0989d32496225ce484c6d75c4
Alicia Moreno <lisireyes93@yahoo.com>
2021-07-25 19:08:32 -0500

    commit tree c0386404c0f513ad681fda770cd02f153ff3c44e

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let para = document.getElementById("text")
+    para.textContent = "This is really cool!"
+    
+  });
+  
